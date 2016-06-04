a52b3b04addbb29a6edf7b2785647b00086b934d
s.prakash <s.prakash10010@gmail.com>
2016-06-04 17:27:31 +0000

    new commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.anantaorganics.in.md-in-1.webhostbox.net/AnantaOrganics/admin/');
+define('HTTP_CATALOG', 'http://www.anantaorganics.in.md-in-1.webhostbox.net/AnantaOrganics/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.anantaorganics.in.md-in-1.webhostbox.net/AnantaOrganics/admin/');
+define('HTTPS_CATALOG', 'http://www.anantaorganics.in.md-in-1.webhostbox.net/AnantaOrganics/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ananthgm/public_html/AnantaOrganics/admin/');
+define('DIR_SYSTEM', '/home/ananthgm/public_html/AnantaOrganics/system/');
+define('DIR_IMAGE', '/home/ananthgm/public_html/AnantaOrganics/image/');
+define('DIR_LANGUAGE', '/home/ananthgm/public_html/AnantaOrganics/admin/language/');
+define('DIR_TEMPLATE', '/home/ananthgm/public_html/AnantaOrganics/admin/view/template/');
+define('DIR_CONFIG', '/home/ananthgm/public_html/AnantaOrganics/system/config/');
+define('DIR_CACHE', '/home/ananthgm/public_html/AnantaOrganics/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/ananthgm/public_html/AnantaOrganics/system/storage/download/');
+define('DIR_LOGS', '/home/ananthgm/public_html/AnantaOrganics/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/ananthgm/public_html/AnantaOrganics/system/storage/modification/');
+define('DIR_UPLOAD', '/home/ananthgm/public_html/AnantaOrganics/system/storage/upload/');
+define('DIR_CATALOG', '/home/ananthgm/public_html/AnantaOrganics/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ananthgm_admin');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'ananthgm_Anantaorganics');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
